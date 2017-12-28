565ddf49c1b4bb12235638e03a85affeb35d8141
49787618 <fernando.maymone@sefaz.ce.gov.br>
2017-12-28 16:10:55 -0300

    added demo.js

--- src/containers/BetBuilder/BetBuilder.js
+++ src/containers/BetBuilder/BetBuilder.js
@@ -22,10 +22,41 @@ import {
     TableHeaderColumn,
     TableRow,
     TableRowColumn,
-  } from 'material-ui/Table';
+    TableCell,
+    TableHead
+
+} from 'material-ui/Table';
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 700,
+    },
+});
+
+
+const data = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+let id = 0;
+function createData(name, calories, fat, carbs, protein) {
+  id += 1;
+  return { id, name, calories, fat, carbs, protein };
+}
 
 
 class BetBuilder extends Component {
+
+
 
     constructor(props) {
         super(props);
@@ -49,28 +80,69 @@ class BetBuilder extends Component {
         matches: []
 
     }
+
+
     render() {
 
         var matches = this.state.matches.map(match =>
-                <Match key={match.id} match={match} />
+            <TableRow>
+                <TableCell>
+                    <Match key={match.id} match={match} />
+                </TableCell>
+            </TableRow>
         );
- 
-        return(
-        <div className={classes.root}>
-            <Grid container spacing={24}>
-                <Grid item xs={12}>
-                    <Table>
-                        <TableBody>
-                            
-                                 {matches}
-                             
-                        </TableBody>
-                    </Table>
+
+        return (
+            /*<div className={classes.root}>
+                <Grid container spacing={24}>
+                    <Grid item xs={12}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Dessert (100g serving)</TableCell>
+                                    <TableCell numeric>Calories</TableCell>
+                                    <TableCell numeric>Fat (g)</TableCell>
+                                    <TableCell numeric>Carbs (g)</TableCell>
+                                    <TableCell numeric>Protein (g)</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+
+                                {matches}
+
+                            </TableBody>
+                        </Table>
+                    </Grid>
                 </Grid>
-            </Grid>
-          
-            
-        </div>
+
+
+            </div>*/
+            <Paper className={classes.root}>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Dessert (100g serving)</TableCell>
+                            <TableCell numeric>Calories</TableCell>
+                            <TableCell numeric>Fat (g)</TableCell>
+                            <TableCell numeric>Carbs (g)</TableCell>
+                            <TableCell numeric>Protein (g)</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.map(n => {
+                            return (
+                                <TableRow key={n.id}>
+                                    <TableCell>{n.name}</TableCell>
+                                    <TableCell numeric>{n.calories}</TableCell>
+                                    <TableCell numeric>{n.fat}</TableCell>
+                                    <TableCell numeric>{n.carbs}</TableCell>
+                                    <TableCell numeric>{n.protein}</TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </Paper>
         )
     }
 
@@ -80,4 +152,4 @@ class BetBuilder extends Component {
 
 
 
-export default BetBuilder;
+export default withStyles(styles)(BetBuilder);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Demo from './demo'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import 'typeface-roboto'
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Demo />, document.getElementById('root'));
 registerServiceWorker();
